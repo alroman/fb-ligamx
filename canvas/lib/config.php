8c596c3910a8641d91d5defb55c603d1b13e6b75
Alfonso Roman <alromanb@gmail.com>
2012-09-20 14:27:33 -0700

    adding some basic fb calls

--- canvas/lib/config.php
+++ canvas/lib/config.php
@@ -3,7 +3,7 @@
 // Files needed for base functionality
 
 #require_once dirname(__FILE__) . '/../config_private.php';
-require_once 'AppInfo.php'
+require_once 'AppInfo.php';
 require_once 'router.php';
 require_once 'controller.php';
 require_once 'model.php';
